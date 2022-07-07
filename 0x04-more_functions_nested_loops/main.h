70b0d924a3d1dc1299579582d8f323746c48b4b8
olayinka03 <aliurobiuo@gmail.com>
2022-07-07 03:54:36 -0700

    add a readme directory and 2 new files

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _putchar - writes the character c to standout
+*/
+int _putchar(char c);
+
+/**
+* _isupper - Write a function that checks for uppercase character
+*/
+int _isupper(int c);
+
+/**
+* _isdigit - Write a function that checks for a digit (0 through 9)
+*/
+int _isdigit(int c);
+
+/**
+* mul - Write a function that multiplies two integers
+*/
+int mul(int a, int b);
+
+/**
+* print_numbers - Write a function that prints the numbers, from 0 to 9, followed by a new line
+*/
+void print_numbers(void);
+
+/**
+* print_most_numbers - Write a function that print numbers from 0 to 9 except 2 and 4
+*/
+void print_most_numbers(void);
+
+/**
+* more_numbers - A function that prints 10 times the numbers form 0 to 14
+*/
+void more_numbers(void);
+
+/**
+* print_line - To draws a straight line in the terminal
+*/
+void print_line(int n);
+
+/**
+* print_diagonal - To draws a diagonal line on the terminal
+*/
+void print_diagonal(int n);
+
+/**
+* print_square - To prints a square followed by a new line
+*/
+void print_square(int size);
+
+/**
+* fizz_buzz - To prints numbers from 1 to 100 followed by a new line
+* for multiples of three print Fizz
+* for multiples of five print Buzz
+* for numbers that are multiples of both three and five print FizzBuzz
+*/
+
+/**
+* print_triangle - To prints a triangle followed by a new line
+*/
+void print_triangle(int size);
+
+#endif /* MAIN_H */
