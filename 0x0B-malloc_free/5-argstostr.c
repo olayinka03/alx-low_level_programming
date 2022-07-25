a05b47adbc9872b42df52840023e8a9e454faeda
olayinka03 <aliurobiuo@gmail.com>
2022-07-25 00:37:50 -0700

    add 2 more files

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* argstostr - concatenates all the arguments of the program
+* @ac: argument count
+* @av: argument vector
+*
+* Return: pointer to new string on success
+* NULL if ac == 0 or av == NULL
+* NULL on fail
+*/
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+
+	str = malloc(sizeof(char) * (size + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	return (str);
+}
