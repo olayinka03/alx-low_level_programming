12f9846c86f18baa6ef5d3ec20f3ac9deac43e7e
olayinka03 <aliurobiuo@gmail.com>
2022-08-24 13:44:33 -0700

    added three files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: name of file
+* @text_content: text to go in file
+*
+* Return: 1 on success, -1 on failure
+* -1 if filename is NULL
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content && text_content[len] != '\0')
+		len++;
+
+	if (!len)
+		write(fd, "", len);
+	else
+		write(fd, text_content, len);
+
+	close(fd);
+
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+# 0x15. C - File I/O
+This Readme contain information on 0x15. C - File I/O and the files that shows example o C - File I/O such as 
+0-read_textfile.c
+1-create_file.c
+2-append_text_to_file.c
+3-cp.c
